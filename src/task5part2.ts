8ca9c328ae7ba8bd38b692aadca05a964ab3d30f
Michał Kubik <bopke2@gmail.com>
2022-12-05 07:06:44 +0100

    Day 5

---
+++ src/task5part2.ts
@@ -0,0 +1,42 @@
+function task5part2() {
+
+    const fs = require('fs');
+    const fileContent = fs.readFileSync("inputs/task5.txt", 'utf-8')
+        .split("\n")
+        .filter((line: string) => line.length > 1);
+
+    let stacksRaw = [] as string[][];
+    fileContent
+        .filter((line: string) => !line.includes("move"))
+        .forEach((line: string) => {
+            for (let i = 0; i < line.length; i++) {
+                if (stacksRaw[i] === undefined) stacksRaw.push([]);
+                stacksRaw[i].push(line[i]);
+            }
+        })
+    let stacks = stacksRaw
+        .map((stack: string[]) => stack.reverse())
+        .filter((stack: string[]) => !isNaN(parseInt(stack[0])))
+        .map((stack: string[]) => stack.join(""))
+        .map((stack: string) => stack.slice(1))
+        .map((stack: string) => stack.trim())
+        .map((stack: string) => [...stack]);
+
+    const number_regex = /(\d+)/g;
+    fileContent.filter((line: string) => line.includes("move")).forEach((line: string) => {
+        const matches = line.match(number_regex)!;
+        const amount = parseInt(matches[0]);
+        const source = stacks[parseInt(matches[1]) - 1];
+        const destination = stacks[parseInt(matches[2]) - 1];
+        const temporaryStack = [] as string[];
+        for (let i = 0; i < amount; i++) {
+            temporaryStack.push(source.pop()!);
+        }
+        for (let i = 0; i < amount; i++) {
+            destination.push(temporaryStack.pop()!)
+        }
+    });
+    return stacks.reduce((accumulator: string, element: string[]) => accumulator += element.at(-1), "");
+}
+
+console.log(task5part2());
