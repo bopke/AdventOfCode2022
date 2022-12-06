1999286c8e941970ec0322d2613b6756914840c5
Michał Kubik <bopke2@gmail.com>
2022-12-06 06:16:27 +0100

    Day 6

---
+++ src/task6.ts
@@ -0,0 +1,35 @@
+function task6() {
+
+    const fs = require('fs');
+    const fileContent = fs.readFileSync("inputs/task6.txt", 'utf-8')
+
+    let queue: string[] = [];
+    let n = 0;
+    [...fileContent]
+        .every((char: string) => {
+            if (queue.length < 3) {
+                n++;
+                queue.push(char);
+                return true;
+            }
+            n++;
+            queue.push(char);
+            let onlyUnique = true;
+            for (let i = 0; i < 4; i++) {
+                for (let j = 0; j < i; j++) {
+                    if (queue[j] == queue[i]) {
+                        onlyUnique = false;
+                    }
+                }
+            }
+            if (onlyUnique) {
+                return false;
+            }
+            queue.shift();
+            return true;
+        })
+
+    return n;
+}
+
+console.log(task6());
